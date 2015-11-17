d25ef8d9147c6c928c831e1314e8aea84bd28b8a
soren <soren@spite.house>
2015-11-16 22:00:06 -0800

    Persicope rotation finessed
    Periscope rotates more slowly, using floats for bearing instead of
    integers
    
    Periscope also accelerates and decelerates smoothly

--- Assets/Scripts/Periscope.cs
+++ Assets/Scripts/Periscope.cs
@@ -9,8 +9,10 @@ public class Periscope : Singleton<Periscope> {
 
 	public enum Direction { N, NE, E, SE, S, SW, W, NW }		// Enumeration for compass directions
 	public Direction DirectionChunk;
-	public int bearing;									// Compass bearing the periscope is facing (0-360)
-	public int chosenBearing; 							// Bearing chosen for exploration
+	public float bearing;									// Compass bearing the periscope is facing (0-360)
+	public float chosenBearing; 							// Bearing chosen for exploration
+	public float rotationSpeed = 0f;
+	public float acc = 2f;
 
 
 	// CLASS METHODS
@@ -39,28 +41,39 @@ public class Periscope : Singleton<Periscope> {
 
 	}
 
+	void adjustVelocity() 
+	// adjusts rotation speed of periscope based on input
+	{
+		if (Input.GetKey("right")) {
+			
+			if (rotationSpeed < 8f) { 
+				rotationSpeed += Time.deltaTime*acc; 
+			}
+			
+		} else if (Input.GetKey("left")) {
+			
+			if (rotationSpeed > -8f) { 
+				rotationSpeed -=Time.deltaTime*acc; 
+			}
+		}
+	}
+
+	void drift()
+	// drifts the periscope after releasing input
+	{
+		if (rotationSpeed > 0f) { rotationSpeed -= Time.deltaTime*acc; if (rotationSpeed < 0f) { rotationSpeed = 0f; } }
+		if (rotationSpeed < 0f) { rotationSpeed += Time.deltaTime*acc; if (rotationSpeed > 0f) { rotationSpeed = 0f; } }
+	}
+
 	void rotatePeriscope()
 	// rotates the periscope
 	{
-	
-		if (Input.GetKey("right")) {
+		bearing += Time.deltaTime*rotationSpeed;
+		if (bearing < 0f) { bearing = 360f; }
+		if (bearing > 360f) { bearing = 0f; }
 
-			if (bearing < 360) { bearing++; }
-			else { bearing = 0; }
-
-			UpdateUI.Instance.bearingIndicator.transform.Rotate(0,0,-1);
-			Camera.main.transform.Rotate(0,1,0);
-
-
-		} else if (Input.GetKey("left")) {
-
-			if (bearing > 0) { bearing--; }
-			else { bearing = 359; }
-
-			UpdateUI.Instance.bearingIndicator.transform.Rotate(0,0,1);
-			Camera.main.transform.Rotate(0,-1,0);
-
-		}
+		UpdateUI.Instance.bearingIndicator.transform.Rotate(0,0,-Time.deltaTime*rotationSpeed);
+		Camera.main.transform.Rotate(0,Time.deltaTime*rotationSpeed,0);
 
 		UpdateUI.Instance.updateDebugText ();
 
@@ -73,9 +86,14 @@ public class Periscope : Singleton<Periscope> {
 	}
 
 	void Update() {
-		if (Input.GetKey ("right") | Input.GetKey ("left")) { rotatePeriscope (); }
+		if (Input.GetKey("right") | Input.GetKey("left")) { adjustVelocity(); }
+		else { if (rotationSpeed != 0f) { drift(); } }
+
+		if (rotationSpeed != 0f) { rotatePeriscope(); } 
+
 		if (Input.GetKey ("up") | Input.GetKey ("down")) { adjustTier(); }
 		if (Input.GetKeyDown ("space")) { commitBearing(); }
+
 
 	}
 }
